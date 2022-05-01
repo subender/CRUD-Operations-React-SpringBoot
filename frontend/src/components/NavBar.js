2de746637719b3c7ce2d201d116eccf17cc85c5f
subender <subender.io@outlook.com>
2022-05-01 15:08:00 +0530

    Created Navigation Bar.

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className=" bg-purple-900 h-16 flex items-center justify-center shadow-lg rounded-b-md">
+        <p className="text-white font-bold uppercase tracking-wider text-xl">Student Data Service</p>
+    </div>
+  )
+}
+
+export default NavBar
