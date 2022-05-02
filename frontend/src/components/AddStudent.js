90e0e462062d430204c146f68a28ec1f80f6d277
subender <subender.io@outlook.com>
2022-05-03 03:09:26 +0530

    connected with backend and add router

--- frontend/src/components/AddStudent.js
+++ frontend/src/components/AddStudent.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import StudentService from '../services/StudentService';
 
 const AddStudent = () => {
+
+    const navigate = useNavigate();
+    const[student, setStudent] = useState({
+        id:"",
+        name:"",
+        address:"",
+        subject:""
+    });
+    
+
+    const changesHandler =(event)=>{
+        const data = event.target.value;
+        setStudent( { ...student, [event.target.name]:data});
+    }
+
+    const onAddStudentHandler =(event)=>{
+        event.preventDefault();
+        StudentService.saveStudent(student).then(response => {
+            navigate("/studentList");
+        }).catch(err =>{
+            console.log(err);
+        })
+    }
+
   return (
     
 <div className="bg-white rounded-lg shadow sm:max-w-md sm:w-full sm:mx-auto sm:overflow-hidden mt-20">
@@ -20,22 +46,23 @@ const AddStudent = () => {
             <div className="w-full space-y-6">
                 <div className="w-full">
                     <div className=" relative ">
-                        <input type="text" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Name"/>
+                        <input type="text" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Name"
+                        name="name" value={student.name} onChange={changesHandler} />
                         </div>
                     </div>
                     <div className="w-full">
                         <div className=" relative ">
-                            <input type="text"  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Address"/>
+                            <input type="text"  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Address" name="address" value={student.address} onChange={changesHandler} />
                             </div>
                         </div>
                         <div className="w-full">
                             <div className=" relative ">
-                                <input type="text"  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="className"/>
+                                <input type="text"  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Favorite  Subject" name="subject" value={student.subject} onChange={changesHandler} />
                                 </div>
                             </div>
                             <div>
                                 <span className="block w-full rounded-md shadow-sm">
-                                    <button type="button" className="py-2 px-4 bg-purple-600 hover:bg-purple-800 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                    <button type="button" className="py-2 px-4 bg-purple-600 hover:bg-purple-800 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg " onClick={onAddStudentHandler} >
                                       Add  +
                                     </button>
                                 </span>
