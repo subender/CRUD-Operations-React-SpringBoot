9331392d6aed551e085dfe608bd1333336710e44
subender <subender.io@outlook.com>
2022-05-03 03:09:56 +0530

    implemented react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import NavBar from "./components/NavBar";
 import AddStudent from "./components/AddStudent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import StudentsList from "./components/StudentsList";
+
 function App() {
   return (
     <>
+    <BrowserRouter> 
+    
     <NavBar/>
-    <AddStudent/>
+    <Routes>
+      <Route index element={<StudentsList/>}/>
+      <Route path="/" element={<StudentsList/>}/> 
+      <Route path="/studentList" element={<StudentsList/>}/>
+      <Route path="/addStudent" element={<AddStudent/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
