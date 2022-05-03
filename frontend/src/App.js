d274b7f5b67c50726694f753992ce00ab95a52cc
subender <subender.io@outlook.com>
2022-05-03 16:19:20 +0530

    added update student form.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import NavBar from "./components/NavBar";
 import AddStudent from "./components/AddStudent";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StudentsList from "./components/StudentsList";
+import EditStudent from "./components/EditStudent";
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Route path="/" element={<StudentsList/>}/> 
       <Route path="/studentList" element={<StudentsList/>}/>
       <Route path="/addStudent" element={<AddStudent/>}/>
+      <Route path="/editStudent/:id" element={<EditStudent/>}/>
     </Routes>
     </BrowserRouter>
     </>
